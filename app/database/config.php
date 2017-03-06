c905e5c7403e3eeefe46d3af3041d4e5a1ac2207
S h i r a n a i <k1r1n@users.noreply.github.com>
2017-03-06 17:42:18 +0700

    Update config.php

--- app/database/config.php
+++ app/database/config.php
@@ -2,10 +2,10 @@
 	ini_set('display_errors', 1);
 	error_reporting(~0);
 	date_default_timezone_set("Asia/Bangkok");
-	define("HOST", "localhost");
-	define("USER", "root");
-	define("PASSWORD", "hme2017");
-	define("DB", "steam");
+	define("HOST", "");
+	define("USER", "");
+	define("PASSWORD", "");
+	define("DB", "");
 	$pdo = new PDO("mysql:host=".HOST.";dbname=".DB.";charset=utf8", USER, PASSWORD);
 	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
